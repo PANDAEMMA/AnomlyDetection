13a543b8b8e14debf2d16189ba377455e0b136cc
b8875 <b8875.tw@gmail.com>
2013-10-16 17:12:17 -0400

    test.pl

--- test.pl
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/perl -w
-#parse the original log to _w.csv,_rw.csv,_r.csv
-
-my $fname ="ktyo.2013";
-open(FH1, $fname.".txt") || die "Cannot open: [FileFiles.pl]$!";
-
-while(<FH1>){
-	print $_
-}
