8464a0aa48b4355515c0386e03d49818c2cfbe4b
b8875 <b8875.tw@gmail.com>
2013-11-09 22:47:05 -0500

    ADParse.py

---
+++ ADApp/ADParse.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+import sys
+import re
+
+MON = 0  # Month index
+DAY = 1  # Day index
+YEAR = 2 # Year index
+HOUR = 4 # Hour index
+MIN = 5  # Minute index
+TEMPER = 8 # Temperature index
+HUMID = 9  # Humidity index
+PRESS = 10  # Pressure index
+
+WEEK_DAY  = 7 # Number of day per week
+# Get data in a specific index
+def get_data_all(fd, flag):
+	arr = []
+	for line in fd.readlines():
+		a = re.split(',|\n| ', line)
+		if (a[flag]):
+			arr.append((float)(a[flag]))
+	return arr
+
+# Counting the number of data hourly, daily, yearly
+def get_num_index(fd, flag):
+	num_index = []
+	count = 0
+	temp = 0
+	for line in fd.readlines():
+		a = re.split(',|\n| ', line)
+		if (temp != (int)(a[flag]) and temp != 0):
+			num_index.append(count)
+			count = 0
+		count = count + 1
+		temp = (int)(a[flag])
+	num_index.append(count)
+	return num_index	
+
+# Get the number of year in the data
+def get_num_year(fd, flag):
+	count = 0
+	temp = 0
+	for line in fd.readlines():
+                a = re.split(',|\n| ', line)
+		if(temp != (int)(a[flag])):
+			count = count + 1
+		temp = (int)(a[flag])
+	return count
+
+# Get weekly index		
+def get_num_week_index(fd, flag):
+	count = 0
+	result = get_num_index(fd, flag)
+	step = WEEK_DAY
+	week_index = []
+	N = len(result)
+	for i in range(0, N, step):
+		if ((N - i) > step):
+			for j in range(step):
+				count = count + result[i + j]
+			week_index.append(count)
+			count = 0
+			
+		else:
+			for k in range(i, N - i, 1):
+				count = count + result[k]
+			if (count != 0):
+				week_index.append(count)
+	return week_index
+
+def get_year_daily_num(a, flag, start, stop):
+        num_index = []
+        count = 0
+        temp = 0
+        for i in range(start, stop):
+                if (temp != (int)(a[flag]) and temp != 0):
+                        num_index.append(count)
+                        count = 0
+                count = count + 1
+                temp = (int)(a[flag])
+        num_index.append(count)
+        return num_index
+
+	
+def get_year_day_num(fd, year_f, day_f, year):
+	year_list = get_num_index(fd, year_f)
+	lines = [year_list[year], year_list[year + 1]]
+	result = get_year_daily_num()
